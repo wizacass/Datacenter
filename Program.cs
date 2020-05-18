acd731087c3bb6dc1846cc9c763a8cdc0330db41
Wizacass <visak.pet0@gmail.com>
2020-05-18 21:59:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Datacenter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
